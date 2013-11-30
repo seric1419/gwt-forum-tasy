b164825f305f87c61ef678367401ff78216871a2
sk42884@st.amu.edu.pl <sk42884@st.amu.edu.pl@5138c663-d315-283a-44aa-0f55ad373d21>
2013-11-30 16:21:34 +0000

    Utworzenie formularza rejestracji

--- trunk/GWTForum/src/com/project/gwtforum/client/GWTForum.java
+++ trunk/GWTForum/src/com/project/gwtforum/client/GWTForum.java
@@ -2,6 +2,9 @@ package com.project.gwtforum.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.project.gwtforum.client.internationalization.GWTForumConstants;
@@ -9,12 +12,15 @@ import com.project.gwtforum.client.widgets.Header;
 
 public class GWTForum implements EntryPoint{
 
-	private GWTForumConstants constants = GWT.create(GWTForumConstants.class);
+	public static GWTForumConstants constants = GWT.create(GWTForumConstants.class);
 	private Header headerWidget = new Header();
 	
 	@Override
 	public void onModuleLoad() {
 		initializeLayout();
+		initializeClickHandlers();
+		
+		History.newItem(headerWidget.getTitle());
 	}
 	
 	private void initializeLayout() {
@@ -22,4 +28,14 @@ public class GWTForum implements EntryPoint{
 		Window.setTitle(constants.windowTitle());
 	}
 
+	private void initializeClickHandlers() {
+		headerWidget.getRegisterButton().addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				RegisterForm form = new RegisterForm();
+				RootPanel.get("contents").add(form.getWidget());
+			}
+		});
+	}
 }
