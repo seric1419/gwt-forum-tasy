bdf13a06608b0c14b6b75158f317a29adf335012
sk42884@st.amu.edu.pl <sk42884@st.amu.edu.pl@5138c663-d315-283a-44aa-0f55ad373d21>
2013-11-30 11:12:27 +0000

    Wrzucenie projektu

---
+++ trunk/GWTForum/src/com/project/gwtforum/client/GWTForumServiceAsync.java
@@ -0,0 +1,5 @@
+package com.project.gwtforum.client;
+
+public interface GWTForumServiceAsync {
+
+}
---
+++ trunk/GWTForum/src/com/project/gwtforum/server/GWTForumServiceImpl.java
@@ -0,0 +1,10 @@
+package com.project.gwtforum.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.project.gwtforum.client.GWTForumService;
+
+public class GWTForumServiceImpl extends RemoteServiceServlet implements GWTForumService{
+
+	private static final long serialVersionUID = 669987357819776292L;
+
+}
